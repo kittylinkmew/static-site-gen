7c964eaaca7a5e3681f1ab47fbc0c6a5ca0a3876
maryk.maddox@gmail.com <maryk.maddox@gmail.com>
2026-05-05 14:41:21 -0400

    TextNode to HTMLNode

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from textnode import TextNode, TextType
+from textnode import TextNode, TextType, text_node_to_html_node
 
 
 class TestTextNode(unittest.TestCase):
@@ -21,5 +21,23 @@ class TestTextNode(unittest.TestCase):
         node8 = TextNode("I'm a test.",TextType.TEXT)
         self.assertEqual(node7,node8)
 
+    def test_text(self):
+        node = TextNode("This is a text node", TextType.TEXT)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, None)
+        self.assertEqual(html_node.value, "This is a text node")
+
+    def test_bold(self):
+        node = TextNode("This is bold.", TextType.BOLD)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag,"b")
+        self.assertEqual(html_node.value, "This is bold.")
+
+    def test_link(self):
+        node = TextNode("Alt text", TextType.IMAGE, "https://images.com")
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag,"img")
+        self.assertEqual(html_node.value, "")
+        self.assertEqual(html_node.props, {"src": "https://images.com","alt":"Alt text"})
 if __name__ == "__main__":
     unittest.main()
