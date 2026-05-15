99bba90443b23135b5dece7ee783b57a17db88ef
maryk.maddox@gmail.com <maryk.maddox@gmail.com>
2026-05-15 17:57:57 -0400

    Split Links Images

--- src/helper.py
+++ src/helper.py
@@ -33,5 +33,65 @@ def extract_markdown_images(text):
 
 #function that takes raw markdown text and returns a list of tuples for links
 def extract_markdown_links(text):
-    matches = re.findall(r"(?<!!)\[([^\[\]]*)\]\(([^\(\)]*)\)",text)
+    matches = re.findall(r"(?<!\!)\[([^\[\]]*)\]\(([^\(\)]*)\)",text)
     return matches
+
+def split_nodes_image(old_nodes):
+    new_nodes = []
+
+    for node in old_nodes:
+        if node.text_type != TextType.TEXT:
+            new_nodes.append(node)
+            continue
+
+        images = extract_markdown_images(node.text)
+
+        if not images:
+            new_nodes.append(node)
+            continue
+
+        current_text = node.text
+        for image_alt, image_link in images:
+            sections = current_text.split(f"![{image_alt}]({image_link})", 1)
+
+            if sections[0] != "":
+                new_nodes.append(TextNode(sections[0],TextType.TEXT))
+
+            new_nodes.append(TextNode(image_alt, TextType.IMAGE, image_link))
+
+            current_text = sections[1]
+
+        if current_text != "":
+            new_nodes.append(TextNode(current_text. TextType.TEXT))
+
+    return new_nodes
+
+def split_nodes_link(old_nodes):
+    new_nodes = []
+
+    for node in old_nodes:
+        if node.text_type != TextType.TEXT:
+            new_nodes.append(node)
+            continue
+
+        links = extract_markdown_links(node.text)
+
+        if not links:
+            new_nodes.append(node)
+            continue
+
+        current_text = node.text
+        for link_alt, link in links:
+            sections = current_text.split(f"[{link_alt}]({link})", 1)
+
+            if sections[0] != "":
+                new_nodes.append(TextNode(sections[0],TextType.TEXT))
+
+            new_nodes.append(TextNode(link_alt, TextType.LINKS, link))
+
+            current_text = sections[1]
+
+        if current_text != "":
+            new_nodes.append(TextNode(current_text. TextType.TEXT))
+
+    return new_nodes
